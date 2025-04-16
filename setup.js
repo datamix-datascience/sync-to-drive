5435d4fa7b8ad2ad16062dc2b1f0e0bc89f3181d
katopz <katopz@gmail.com>
2025-04-16 15:20:02 +0900

    Prepare release v1

--- setup.js
+++ setup.js
@@ -1,19 +1,22 @@
-const { execSync } = require("child_process");
-const path = require("path");
+import { execSync } from "child_process";
+import { resolve } from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = resolve(fileURLToPath(import.meta.url), "..");
 
 try {
   console.log("Installing additional dependencies...");
   execSync("cat package.json", {
     stdio: "inherit",
-    cwd: path.resolve(__dirname),
+    cwd: resolve(__dirname),
   });
   execSync("npm install", {
     stdio: "inherit",
-    cwd: path.resolve(__dirname),
+    cwd: resolve(__dirname),
   });
   execSync("ls -R dist", {
     stdio: "inherit",
-    cwd: path.resolve(__dirname),
+    cwd: resolve(__dirname),
   });
   console.log("Dependencies installed successfully.");
 } catch (error) {
